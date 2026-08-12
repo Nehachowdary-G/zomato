b30f4d9ec63b6d53340c6df9200633af3ab6cc79
Nehachowdary-G <nehachowdarygorjala@gmail.com>
2026-08-12 11:19:13 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -38,11 +38,11 @@ const Header = () => {
         <h3>Discover the best food & drinks in Patna</h3>
         <div className="input">
           <select name="" id="">
-            <option value="Chennai">Chennai</option>
-            <option value="Jaipur">Jaipur</option>
-            <option value="Delhi">Delhi</option>
-            <option value="Mumbai">Mumbai</option>
-            <option value="Kolkata">Kolkata</option>
+            <option value="Ap">AP</option>
+            <option value="Karnataka">Karnataka</option>
+            <option value="TamilNadu">TamilNadu</option>
+            <option value="Maharastra">Maharastra</option>
+            <option value="pune">pune</option>
           </select>
           |
           <input
